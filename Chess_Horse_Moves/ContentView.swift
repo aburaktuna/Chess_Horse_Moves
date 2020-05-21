bf181dc9c8c97a789b725dbc54282f0ece527e26
Burak A. TUNA <aburaktuna@gmail.com>
2020-05-21 23:03:22 +0300

    Initial Commit

---
+++ Chess_Horse_Moves/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chess_Horse_Moves
+//
+//  Created by Burak TUNA on 21.05.2020.
+//  Copyright © 2020 Burak TUNA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
